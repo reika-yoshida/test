2bdee0a43809478773a5b91ba70ecd3f2711e782
reika <rkishinaga@gmail.com>
2023-04-07 12:27:03 +0900

    自己紹介文

---
+++ HelloWorld/src/selfIntroduction.java
@@ -0,0 +1,24 @@
+
+public class selfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		氏名
+		String name = "吉田　玲香";
+//		年齢
+		int age = 26;
+//　	　　趣味
+		String hobby = "映画鑑賞";
+
+/*
+ * 　　	自己紹介文を
+ *		出力する処理
+ */
+		
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
