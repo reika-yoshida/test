fe94861918e6327f4331022a9349c0ef09fa7012
reika <rkishinaga@gmail.com>
2023-04-26 21:51:42 +0900

    課題1_16修正いたしました【再提出】

--- HelloWorld/src/curriculum_New_question/Curriculum_New_1_16.java
+++ HelloWorld/src/curriculum_New_question/Curriculum_New_1_16.java
@@ -18,13 +18,13 @@ public class Curriculum_New_1_16 {
 		System.out.println(d);
 		// Q5：変数numsに「10, 100, 1000」が格納された配列を代入して、コンソールにメモリの番地が1のデータを出力してください。
 		int[] nums = {10,100,1000};
-		System.out.println(nums[0]);
+		System.out.println(nums[1]);
 		// Q6：Q5で作成した変数numsを変数bに代入し、その後変数bのメモリの番地が1のデータを「8」に上書きさせて
 		//     コンソールに変数numsと変数bのメモリの番地が1のデータを出力させて、変数nums値が変わることを確認してください。
 		int[] b = nums;
 		b[0] = 8;
-		System.out.println(nums[0]);
-		System.out.println(b[0]);
+		System.out.println(nums[1]);
+		System.out.println(b[1]);
 	}
 
 }
