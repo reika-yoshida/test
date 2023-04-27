fe22c686bdc314033a340b9ba46d7c1b71bed79a
reika <rkishinaga@gmail.com>
2023-04-27 21:50:40 +0900

    課題1_16【再提出】

--- HelloWorld/src/curriculum_New_question/Curriculum_New_1_16.java
+++ HelloWorld/src/curriculum_New_question/Curriculum_New_1_16.java
@@ -22,7 +22,7 @@ public class Curriculum_New_1_16 {
 		// Q6：Q5で作成した変数numsを変数bに代入し、その後変数bのメモリの番地が1のデータを「8」に上書きさせて
 		//     コンソールに変数numsと変数bのメモリの番地が1のデータを出力させて、変数nums値が変わることを確認してください。
 		int[] b = nums;
-		b[0] = 8;
+		b[1] = 8;
 		System.out.println(nums[1]);
 		System.out.println(b[1]);
 	}
