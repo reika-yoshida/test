b5d62ac413d11bfa98a2db9296b87e1e6c0e4eb7
reika <rkishinaga@gmail.com>
2023-05-10 22:52:59 +0900

    課題1_22【新規提出】

---
+++ HelloWorld/src/curriculum_New_question/javaproject/Example.java
@@ -0,0 +1,12 @@
+package curriculum_New_question.javaproject;
+// 実行
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Process process = new Process("こんにちは！　ここは日本です！","ここの寿司はうまい","寿司は和食です");
+		// コンソール出力
+		process.priDate();
+	}
+
+}
