18af535e071b60ed1747b7d2e791356e265c2492
reika <rkishinaga@gmail.com>
2023-05-22 22:21:35 +0900

    課題1_25【新規提出】

---
+++ HelloWorld/src/curriculum_New_question/curriculum1_25/process/Player.java
@@ -0,0 +1,23 @@
+package curriculum_New_question.curriculum1_25.process;
+
+import java.util.Random;
+
+
+//サブクラス
+public class Player extends Status{
+	//ランダムな数字を生成
+	public int Random() {
+		Random random = new Random();
+		int rand = random.nextInt(1000)+1;
+		return rand;
+	}
+	
+	//　サブクラスのコンストラクタ
+	public Player() {
+		this.setHp(this.Random());
+		this.setMp(this.Random());
+		this.setAttack(this.Random());
+		this.setSpeed(this.Random());
+		this.setDefense(this.Random());
+	}
+}
