1d780fae685e779900959a68387e5a644804d025
reika <rkishinaga@gmail.com>
2023-06-06 22:22:34 +0900

    課題1_27【新規提出】

---
+++ HelloWorld/src/curriculum_New_question/curriculum1_27/Example.java
@@ -0,0 +1,18 @@
+package curriculum_New_question.curriculum1_27;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		// コンソール出力
+		System.out.println("コンソールに文字を入力してください");	
+		
+		// インスタンスに変数を代入
+		Process process = new Process();
+		Process process2 = new Process();
+		
+		// コンソール出力
+		process.priDate();
+		process2.priDate();
+	}
+}
---
+++ HelloWorld/src/curriculum_New_question/curriculum1_27/Process.java
@@ -0,0 +1,42 @@
+package curriculum_New_question.curriculum1_27;
+import java.util.Scanner;
+
+public class Process {
+
+	public void priDate() {
+		
+		// インスタンスにscannerを代入
+		Scanner scanner = new Scanner(System.in);
+		String aa = scanner.next();
+
+		// aaをカンマ区切りでarraysに格納
+		String[] arrays = aa.split(",");
+
+		// arraysを繰り返し出力
+		for (String array : arrays) {
+			// bbに”：”区切りで格納
+			String[] bb = array.split(":");
+
+			// コンソール出力
+			System.out.println("");
+			System.out.println("動物名：" + bb[0]);
+			System.out.println("体長：" + bb[1] + "m");
+			System.out.println("速度：" + bb[2] + "km/h");
+
+			// if文で学名のコンソール出力
+			if (bb[0].equals("ライオン")) {
+				System.out.println("学名：パンテラ レオ");
+			}else if(bb[0].equals("ゾウ")) {
+				System.out.println("学名：ロキソドンタ・サイクロティス");
+			}else if(bb[0].equals("パンダ")) {
+				System.out.println("学名：アイルロポダ・メラノレウカ");
+			}else if(bb[0].equals("チンパンジー")) {
+				System.out.println("学名：パン・トゥログロディテス");
+			}else if(bb[0].equals("シマウマ")) {
+				System.out.println("学名：チャップマンシマウマ");
+			}else if(bb[0].equals("インコ")) {
+				System.out.println("学名：不明");
+			}
+		}
+	}
+}
