1799db86c3356aa3c54fdf647f7142a475324978
reika <rkishinaga@gmail.com>
2023-06-14 22:46:24 +0900

    課題1_30【再提出】

--- HelloWorld/src/curriculum_New_question/curriculum_1_30/Main.java
+++ HelloWorld/src/curriculum_New_question/curriculum_1_30/Main.java
@@ -2,14 +2,9 @@ package curriculum_New_question.curriculum_1_30;
 
 class Main {
 	public static void main(String[] args) {
-		Person person = new Person("鈴木太郎", 20, 1.7,60);
-
-		System.out.println(person.name);
-		System.out.println(person.age);
-		System.out.println(person.height);
-		System.out.println("");
+		Person person = new Person("鈴木太郎", 20, 1.7,60,10);
 
 		// print呼び出し
-		person.print();
+		Person.print();
 	}
 }
--- HelloWorld/src/curriculum_New_question/curriculum_1_30/Person.java
+++ HelloWorld/src/curriculum_New_question/curriculum_1_30/Person.java
@@ -2,30 +2,33 @@ package curriculum_New_question.curriculum_1_30;
 
 class Person{
 	// インスタンスフィールドを定義
-	public String name;
-	public int age;
-	public double height;
-	public double weight;
+	public static String name;
+	public static int age;
+	public static double height;
+	public static double weight;
+	public static int num;
 
 	// コンストラクタを定義しインスタンスフィールドに値をセット
-	Person(String name, int age, double height, double weight){
-		this.name = name;
-		this.age = age;
-		this.height = height;
-		this.weight = weight;
+	Person(String name, int age, double height, double weight, int num){
+		Person.name = name;
+		Person.age = age;
+		Person.height = height;
+		Person.weight = weight;
+		Person.num = num;
 	}
 
 	// BMI
-	public double bmi() {
+	public static double bmi() {
 		double bmi = weight / (height * height);
 		return bmi;
 	}
+	
 
 	// コンソール出力
-	public void print() {
-		System.out.println("名前は" + this.name + "です");
-		System.out.println("年は" + this.age + "です");
-		System.out.println("BMIは" + (String.format("%.1f",this.bmi())) + "です");
-		System.out.println("合計○人です");
+	public static void print() {
+		System.out.println("名前は" + name + "です");
+		System.out.println("年は" + age + "です");
+		System.out.println("BMIは" + (String.format("%.1f",bmi())) + "です");
+		System.out.println("合計" + num + "人です");
 	}
 }
