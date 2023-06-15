9208e5a84843a1ae1d869dd171fc4bd5147d0014
reika <rkishinaga@gmail.com>
2023-06-15 21:37:43 +0900

    課題1_30【再提出】

--- HelloWorld/src/curriculum_New_question/curriculum_1_30/Main.java
+++ HelloWorld/src/curriculum_New_question/curriculum_1_30/Main.java
@@ -2,9 +2,12 @@ package curriculum_New_question.curriculum_1_30;
 
 class Main {
 	public static void main(String[] args) {
-		Person person = new Person("鈴木太郎", 20, 1.7,60,10);
+		// 問題5：Main.javaの引数にweightの60を入れてください
+		Person person = new Person("鈴木太郎", 20, 1.7,60);
 
 		// print呼び出し
-		Person.print();
+		person.print();
+		// 問題10：人数の合計を「合計○人です」と出力してください。
+		System.out.println("合計" + person.num + "人です");
 	}
 }
--- HelloWorld/src/curriculum_New_question/curriculum_1_30/Person.java
+++ HelloWorld/src/curriculum_New_question/curriculum_1_30/Person.java
@@ -1,34 +1,37 @@
 package curriculum_New_question.curriculum_1_30;
 
 class Person{
-	// インスタンスフィールドを定義
-	public static String name;
-	public static int age;
-	public static double height;
-	public static double weight;
-	public static int num;
+	// 問題1：インスタンスフィールドを定義してください
+	public  String name;
+	public int age;
+	public double height;
+	// 問題4：インスタンスフィールド「weight」を定義し、コンストラクタの中で値をセットしてください（double型）
+	public double weight;
+	public int num = 0;
 
-	// コンストラクタを定義しインスタンスフィールドに値をセット
-	Person(String name, int age, double height, double weight, int num){
-		Person.name = name;
-		Person.age = age;
-		Person.height = height;
-		Person.weight = weight;
-		Person.num = num;
+	// 問題2：コンストラクタを定義してください
+	// 問題3：コンストラクタの中で各インスタンスフィールドに値をセットしてください
+	Person(String name, int age, double height, double weight){
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		num++;
 	}
 
-	// BMI
-	public static double bmi() {
-		double bmi = weight / (height * height);
+	// 問題6：インスタンスメソッド「bmi」を定義してください（戻り値：double）
+	public double bmi() {
+		double bmi = weight / height / height;
+		// 問題7：bmiメソッドでインスタンスのBMIを返すようにしてください
 		return bmi;
 	}
 	
 
-	// コンソール出力
-	public static void print() {
-		System.out.println("名前は" + name + "です");
-		System.out.println("年は" + age + "です");
-		System.out.println("BMIは" + (String.format("%.1f",bmi())) + "です");
-		System.out.println("合計" + num + "人です");
+	// 問題8：インスタンスメソッド「print」を定義してください（戻り値：void）
+	public void print() {
+		// 問題9：printメソッドの中でthisを用いて「名前は〇〇です」,「年は〇〇です」「BMIは○○です」と出力してください
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this.age + "です");
+		System.out.println("BMIは" + Math.floor(bmi()) + "です");
 	}
 }
