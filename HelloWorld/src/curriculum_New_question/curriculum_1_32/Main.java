f54a510a3e6f582450e5bb464302e623d1e32146
reika <rkishinaga@gmail.com>
2023-06-16 11:00:04 +0900

    課題1_32【新規提出】

---
+++ HelloWorld/src/curriculum_New_question/curriculum_1_32/Main.java
@@ -0,0 +1,17 @@
+package curriculum_New_question.curriculum_1_32;
+
+class Main{
+	public static void main(String[] argos){
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+		person1.print();
+		
+		// 問題3：Main.javaで System.out.printlnを使い「合計〇〇人です」と出力してください。〇〇はcountの値です。
+		System.out.println("合計" + Person.count + "人です");
+
+		Person person2 = new Person("山田花子", 22, 1.5, 40);
+		person2.print();
+		
+		// 問題6：クラスメソッド「printCount」を呼び出してください
+		person1.printCount();
+	}
+}
---
+++ HelloWorld/src/curriculum_New_question/curriculum_1_32/Person.java
@@ -0,0 +1,39 @@
+package curriculum_New_question.curriculum_1_32;
+
+class Person{
+	// インスタンスフィールドを定義
+	public String name;
+	public int age;
+	public double height;
+	public double weight;
+	// 問題1：クラスフィールド「count」を定義してください（初期値：0,データ型：int）
+	public static int count = 0;
+
+	
+	Person(String name, int age, double height, double weight){
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		// 問題2：Personコンストラクタの中でクラスフィールドcountに1を足してください
+		Person.count++;
+	}
+	
+	// BMI
+	public double bmi(){
+		return this.weight / height / height;
+	}
+
+	// コンソール出力
+	public void print(){
+		System.out.println("名前は" + name + "です");
+		System.out.println("年は" + age + "です");
+		System.out.println("BMIは" + Math.floor(bmi()) + "です");
+	}
+	
+	// 問題4：クラスメソッド「printCount」を定義してください（データ型：void）
+	public void printCount() {
+		// 問題5：クラスメソッドの中にクラスフィールド「count」を用いて「合計〇〇人です」と出力してください
+		System.out.println("合計" + Person.count + "人です");
+	}
+}
